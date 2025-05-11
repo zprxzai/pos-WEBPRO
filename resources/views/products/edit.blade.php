170d02e4ecc31bb2b14d7b8f50d0db21133b7bb5
zaidan pasya <zpr7002@gmail.com>
2025-05-11 11:34:25 +0700

    perbaikan

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -15,7 +15,7 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('categories.index') }}">Kategori</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('products.index') }}">Produk</a></li>
                             <li class="breadcrumb-item active">Edit</li>
                         </ol>
                     </div>
